68ecf926fd6051d79c4ce80494d9d901a5c08767
happybithdaymeena <githubtrial001@gmail.com>
2020-12-18 12:59:13 +0530

    First Commit

---
+++ songScript.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    var sound = new Audio;
+    sound.src = "imagesAndMedia/HappyBirthdayOrchestraEdited.mp3"
+    var delayInMilliSeconds = 20500;
+    setTimeout(function(){sound.play();},delayInMilliSeconds);
+}
