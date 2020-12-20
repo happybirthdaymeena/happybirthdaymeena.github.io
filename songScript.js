712917f7d4e7baa2651c33d3279f2b4948b02c45
happybithdaymeena <githubtrial001@gmail.com>
2020-12-20 19:35:52 +0530

    increased display time for bubble-2

--- songScript.js
+++ songScript.js
@@ -1,6 +1,6 @@
 function myFunction(){
     var sound = new Audio;
     sound.src = "imagesAndMedia/HappyBirthdayOrchestraEdited.mp3"
-    var delayInMilliSeconds = 28500;
+    var delayInMilliSeconds = 30500;
     setTimeout(function(){sound.play();},delayInMilliSeconds);
 }
